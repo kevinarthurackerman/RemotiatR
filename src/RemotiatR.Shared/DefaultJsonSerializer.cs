ab09c118a46b255768105912318b224c316b9040
kackerman <kevin.arthur.ackerman@gmail.com>
2020-02-06 19:59:42 -0600

    Fixes characters preceeding json message

--- src/RemotiatR.Shared/DefaultJsonSerializer.cs
+++ src/RemotiatR.Shared/DefaultJsonSerializer.cs
@@ -17,7 +17,7 @@ namespace RemotiatR.Shared
         public object Deserialize(Stream stream, Type type)
         {
             if (stream.CanSeek) stream.Seek(0, SeekOrigin.Begin);
-            using var streamReader = new StreamReader(stream, Encoding.UTF8);
+            using var streamReader = new StreamReader(stream, Encoding.Default);
             using var jsonTextReader = new JsonTextReader(streamReader);
             return _jsonSerializer.Deserialize(jsonTextReader, type);
         }
@@ -25,7 +25,7 @@ namespace RemotiatR.Shared
         public Stream Serialize(object value, Type type)
         {
             var stream = new MemoryStream();
-            using var streamWriter = new StreamWriter(stream, Encoding.UTF8, 1024, true);
+            using var streamWriter = new StreamWriter(stream, Encoding.Default, 1024, true);
             using var jsonTextWriter = new JsonTextWriter(streamWriter);
             jsonTextWriter.CloseOutput = false;
             _jsonSerializer.Serialize(jsonTextWriter, value, type);
