7411edc09235365f586bdef49a7b6f26e3a4102d
kackerman <kevin.arthur.ackerman@gmail.com>
2020-03-08 22:12:51 -0500

    Removes unused variable

--- src/RemotiatR.Shared/Internal/RequestUriBuilder.cs
+++ src/RemotiatR.Shared/Internal/RequestUriBuilder.cs
@@ -5,8 +5,6 @@ namespace RemotiatR.Shared.Internal
 {
     internal static class RequestUriBuilder
     {
-        private static readonly ConcurrentDictionary<Type, Uri> _uriCache = new ConcurrentDictionary<Type, Uri>();
-
         internal static Func<Type, Uri> DefaultUriBuilder = x => new Uri("/api/" + x.FullName.Replace('.', '/').Replace('+', '-'), UriKind.Relative);
     }
 }
