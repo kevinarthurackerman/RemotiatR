9ec3515856b151e6979972d619f748aadc8354aa
kackerman <kevin.arthur.ackerman@gmail.com>
2020-03-08 22:12:14 -0500

    Removes uri caching since they are cached later on

--- src/RemotiatR.Shared/Internal/RequestUriBuilder.cs
+++ src/RemotiatR.Shared/Internal/RequestUriBuilder.cs
@@ -7,14 +7,6 @@ namespace RemotiatR.Shared.Internal
     {
         private static readonly ConcurrentDictionary<Type, Uri> _uriCache = new ConcurrentDictionary<Type, Uri>();
 
-        internal static Func<Type, Uri> DefaultUriBuilder = x => 
-            _uriCache.GetOrAdd(x, x =>
-            {
-                if (x == null) throw new ArgumentNullException(nameof(x));
-
-#pragma warning disable CS8602 // Dereference of a possibly null reference.
-                return new Uri("/api/" + x.FullName.Replace('.', '/').Replace('+', '-'), UriKind.Relative);
-#pragma warning restore CS8602 // Dereference of a possibly null reference.
-            });
+        internal static Func<Type, Uri> DefaultUriBuilder = x => new Uri("/api/" + x.FullName.Replace('.', '/').Replace('+', '-'), UriKind.Relative);
     }
 }
