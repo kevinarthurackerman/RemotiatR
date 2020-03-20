6d41b0e423b9d129722671d5094e7793d6bd4ebe
kackerman <kevin.arthur.ackerman@gmail.com>
2020-03-19 22:48:03 -0500

    Refactors shared code between handlers

---
+++ src/Core/RemotiatR.Core.Client/BaseMessageHandler.cs
@@ -0,0 +1,33 @@
+﻿using RemotiatR.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace RemotiatR.Client
+{
+    internal abstract class BaseMessageHandler
+    {
+        private readonly IMessageTransport _messageTransport;
+        private readonly IEnumerable<IMessagePipelineHandler> _messageHandlers;
+        private readonly Uri _uri;
+
+        public BaseMessageHandler(IMessageTransport messageTransport, IEnumerable<IMessagePipelineHandler> messageHandlers, Uri uri)
+        {
+            _messageTransport = messageTransport ?? throw new ArgumentNullException(nameof(messageTransport));
+            _messageHandlers = messageHandlers ?? throw new ArgumentNullException(nameof(messageHandlers));
+            _uri = uri ?? throw new ArgumentNullException(nameof(uri));
+        }
+
+        protected MessagePipelineDelegate BuildHandler(CancellationToken cancellationToken)
+        {
+            var terminalHandler = (MessagePipelineDelegate)(async message => await _messageTransport.SendRequest(_uri, message, cancellationToken));
+
+            var handle = _messageHandlers
+                .Reverse()
+                .Aggregate(terminalHandler, (next, outerHandle) => async message => await outerHandle.Handle(message, next, cancellationToken));
+
+            return handle;
+        }
+    }
+}
