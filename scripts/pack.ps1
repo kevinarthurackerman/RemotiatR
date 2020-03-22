a76cba8919018f802760371d53ae4b4e3106688d
kackerman <kevin.arthur.ackerman@gmail.com>
2020-03-22 10:19:20 -0500

    Updates pack script to clear packages in local cache

--- scripts/pack.ps1
+++ scripts/pack.ps1
@@ -2,33 +2,56 @@ Add-Type -AssemblyName System.IO.Compression.FileSystem
 
 $root=(Get-Item -Path ".\..").FullName
 
+Write-Output "Cleaning up and creating build directories"
+
 $scriptsDir = "$($root)\scripts"
 $nugetDir = "$($root)\nuget"
 $packagesDir = "$($root)\packages"
 $symbolsDir = "$($root)\symbols"
 
 if(Test-Path $nugetDir){
+    Write-Output "  Cleaning $($nugetDir)"
     Remove-Item $nugetDir\* -Recurse
 }
 else {
+    Write-Output "  Creating $($nugetDir)"
     New-Item -ItemType Directory -Path $nugetDir | Out-Null 
 }
 
 if(Test-Path $packagesDir){
+    Write-Output "  Cleaning $($packagesDir)"
     Remove-Item $packagesDir\* -Recurse
 }
 else {
+    Write-Output "  Creating $($packagesDir)"
     New-Item -ItemType Directory -Path $packagesDir | Out-Null 
 }
 
 if(Test-Path $symbolsDir){
+    Write-Output "  Cleaning $($symbolsDir)"
     Remove-Item $symbolsDir\* -Recurse
 }
 else {
+    Write-Output "  Creating $($symbolsDir)"
     New-Item -ItemType Directory -Path $symbolsDir | Out-Null 
 }
 
 . "$($scriptsDir)\variables.ps1"
+
+Write-Output "Checking for existing nuget packages to remove"
+$buildOrder | ForEach-Object {
+    . "$($scriptsDir)\variables.ps1"
+    . "$($scriptsDir)\$($_).ps1"
+
+    $packagePath = "$($env:USERPROFILE)\.nuget\packages\$($packageId)\$($version)"
+
+    if(Test-Path $packagePath){
+        Write-Output "  Removing package $($packagePath)"
+        Remove-Item $packagePath -Recurse
+    }
+}
+
+Write-Output "Building nuget packages"
 $buildOrder | ForEach-Object {
     . "$($scriptsDir)\variables.ps1"
     . "$($scriptsDir)\$($_).ps1"
@@ -70,3 +93,5 @@ $buildOrder | ForEach-Object {
     }
     $archive.Dispose()
 }
+
+Write-Output "Pack complete!"
